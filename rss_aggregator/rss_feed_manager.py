92fba0f6a6c567f3685b0ed801e8355ad9f06475
Mike Beaumier <michael.beaumier@gmail.com>
2017-08-18 13:57:20 -0700

    got both feed thread and processing thread finished

--- rss_aggregator/rss_feed_manager.py
+++ rss_aggregator/rss_feed_manager.py
@@ -1,7 +1,8 @@
 import feedparser
+import requests
 
-import feeds
-
+from io import BytesIO
+import json
 import copy
 import re
 import html
@@ -11,9 +12,14 @@ import multiprocessing
 from threading import Thread
 from queue import Queue
 
-logging.basicConfig(level=logging.DEBUG)
+## TODO
+# 1. Multithreading - Everything writes to the queue
+# 2. Separate thread reads from queue and updates a database
 
-q = Queue()
+logging.basicConfig(level=logging.DEBUG)
+news_queue = multiprocessing.Queue() # must be multiprocessing queue to share
+                                     # queue across processes
+control_queue = Queue()
 
 def santize_html(s):
     """Take an input string s, find all things that look like SGML character
@@ -51,6 +57,7 @@ class RSSFeed(object):
         self.key = key
         self.data = None
         self.last_parsed = None
+        self.message = None
 
     def __repr__(self):
         return "key {}, url {}, #stories {}".format(self.key, self.url, len(self.data['entries']))
@@ -68,48 +75,57 @@ class RSSFeedManager(object):
     def add_feed(self, url, key=None):
         if key is None:
             key = url
-            logging.info("Adding feed {} to update every {} seconds".format(url, key, self.update_interval))
-        logging.info("Adding feed {} with key {} to update every {} seconds".format(url, key, self.update_interval))
+            logging.info("RSSFeedManager.add_feed: key:{}, url:{}, update_interval:{}".format(url, key, self.update_interval))
+        logging.info("RSSFeedManager.add_feed: key:{}, url:{}, update_interval:{}".format(url, key, self.update_interval))
         self.feeds[key] = RSSFeed(url, key)
+        return
 
     def download_feed(self, feed):
         '''
         :param feed: an RSSFeed type object
+        :param news_queue: put news on the queue for processing
         '''
-        logging.info("Downloading feed {}".format(feed.key))
-        feed.data = feedparser.parse(feed.url)
-        return feed
+        global news_queue
+
+        try:
+            resp = requests.get(feed.url, timeout=20.0) 
+        except requests.ReadTimeout:
+            logging.warn("RSSFeedManager.download_feed: timeout, key:{}".format(feed.key))
+            return
+        content = BytesIO(resp.content)
+        feed.data=feedparser.parse(content)
+        feed.message="SUCCESS"
+        logging.info("RSSFeedManager.download_feed: stories:{}, key:{}".format(len(feed.data['entries']), feed.key))
+        news_queue.put(feed)
     
     # run feed updater on its own thread
     def get_feeds(self):
-        global q
         # add means to terminate updating.
+        global news_queue
         while True:
-            logging.info("Updating feeds, update interval {}".format(self.update_interval))
-            out = None
+            logging.info("RSSFeedManager.get_feeds updating".format(self.update_interval))
             with multiprocessing.Pool(processes=self.num_processes) as pool:
                 results = pool.map_async(self.download_feed, list(self.feeds.values()))
                 results.wait()
-                out = results.get() # array of feed objects
-                for result in out:
-                    logging.info('Got result {}'.format(result))
-            q.put(out)
+            logging.info("RSSFeedManager.get_feeds sleeping")
             time.sleep(self.update_interval)
 
     def echo_results(self):
-        global q
+        global news_queue
+
         while True:
-            self.test_state.append(q.get())
-            time.sleep(2)
-            logging.info('STATE CHECK {}, Queue State {}'.format(len(self.test_state), q.empty()))
-            time.sleep(3)
+            data = news_queue.get()
+            logging.info('GOT {}'.format(data))
+            self.test_state.append(data)
+            logging.info('RSSFeedManager.echo_results, test_state:{}, news_queue.empty():{}'.format(len(self.test_state), news_queue.empty()))
 
 if __name__ == "__main__":
-    r = RSSFeedManager(feed_list=feeds.feed_list, update_interval=3, num_processes=4)
+    with open('feeds.json','r') as f:
+        feeds = json.load(f)
+
+    r = RSSFeedManager(feed_list=feeds, update_interval=30, num_processes=4)
     t = Thread(target=r.get_feeds, args=())
     t.start()
+
     t2 = Thread(target=r.echo_results, args=())
     t2.start()
-
-    time.sleep(10)
-    print("OUTSIDE SHIT!", r.test_state)
